727a910714926b6041ad75fdf53fcaeb6a73a23a
Kelly Hum <kellyhum88@gmail.com>
2021-06-26 16:01:40 -0700

    changed code to match video; see notes in readme

--- 1 - js drumkit/script.js
+++ 1 - js drumkit/script.js
@@ -1,67 +1,17 @@
 /**
  * script.js
  * contains the keybindings that make the audio run in js drumkit
- * TODO: figure out shorter/more elegant code for audio rewind
+ * TODO: rewrite program into my own version
  */
 
-/*event listener for when a key is pressed*/
-window.addEventListener('keydown', a => {
+const playSound = (a) => {
+    const audio = document.querySelector(`audio[data-letter="${a.key}"]`); //selects specific audio elements
+    const item = document.querySelector(`.grid-item[data-letter="${a.key}"]`); //selects specific grid item element
+    if (!audio) return; //breaks code
+    audio.currentTime = 0; //reverts audio file back to 0:00
+    audio.play(); //plays audio files
+    
+    item.classList.toggle('selected'); //toggles class
+}
 
-    if (a.key=="a") {
-        let audio = document.querySelector('#clap');
-        if (!audio) return; //stops function
-        audio.currentTime = 0; //rewinds to beginning of sound
-        audio.play();
-
-    } else if (a.key=="s") {
-        let audio = document.querySelector('#hihat');
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-
-    } else if (a.key=="d") {
-        let audio = document.querySelector('#kick');
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-
-    } else if (a.key=="f") {
-        let audio = document.querySelector('#openhat');
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-
-    } else if (a.key=="g") {
-        let audio = document.querySelector('#boom');
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-
-    } else if (a.key=="h") {
-        let audio = document.querySelector('#ride');
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-        
-    } else if (a.key=="j") {
-        let audio = document.querySelector('#snare');
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-
-    } else if (a.key=="k") {
-        let audio = document.querySelector('#tom');
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-
-    } else if (a.key=="l") {
-        document.querySelector('#tink').play();
-    }
-});
-
-let gridItem = document.querySelector('.grid-item');
-
-document.addEventListener('keydown', function () {
-    this.classList.toggle('selected');
-});
+window.addEventListener('keydown', playSound); //event listener for keypresses
