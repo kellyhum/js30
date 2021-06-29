6407b9c96cf0ffa615505bfa5e33949bfa331d77
Kelly Hum <kellyhum88@gmail.com>
2021-06-28 19:59:22 -0700

    Update script.js

--- 3 - js and css variables/script.js
+++ 3 - js and css variables/script.js
@@ -2,3 +2,14 @@
  * script.js
  * used for css and js project
  */
+
+//Functions
+const inputs = document.querySelectorAll('#commands input');
+
+function update() {
+    const element = this.dataset.size || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + element);
+}
+
+inputs.forEach(inputs => inputs.addEventListener('change', update));
+inputs.forEach(inputs => inputs.addEventListener('mousemove', update));
