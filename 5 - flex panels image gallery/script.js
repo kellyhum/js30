fa4007163eaff930591b16a5d3c1ec3e35b6a1b8
Kelly Hum <kellyhum88@gmail.com>
2021-07-01 19:08:55 -0700

    added class toggle code

--- 5 - flex panels image gallery/script.js
+++ 5 - flex panels image gallery/script.js
@@ -3,3 +3,17 @@
  * used to toggle classes in the flex panels image gallery
  */
 
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(a) {
+    if (a.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
